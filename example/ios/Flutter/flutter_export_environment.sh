3ffe03c0127ec4f267eded3b5b6a1a205b275bc3
mountain <mountain@192.168.0.101>
2020-04-25 10:04:08 +0800

    fix input cursor on iOS

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Flutter\flutter_source\1.12.13"
-export "FLUTTER_APPLICATION_PATH=E:\Flutter\FlutterCandies\dev\extended_text_field\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/mountain/Documents/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mountain/Documents/consult/code/extended_text_field/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=E:\Flutter\flutter_source\1.12.13\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mountain/Documents/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
